4036757055f917c4b032e336a7732696a76f208f
Artem <artuom-paul@mail.ru>
2023-02-01 10:36:54 +0800

    python2

---
+++ LSN2/001.py
@@ -0,0 +1,12 @@
+a = 'hello'
+for i in a: 
+    print(i)
+
+# for e in range(2, 10, 2):     # начальный элемент, конечный, шаг
+#     print(e)
+
+for ind in range(len(a)):
+    print(a[ind])
+
+for _ in range (3):
+    print('Hello')
