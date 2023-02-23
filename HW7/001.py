69de586c2766b38ac1d9234fe316421629c6b221
Artem <artuom-paul@mail.ru>
2023-02-23 13:17:09 +0800

    python7

--- HW7/001.py
+++ HW7/001.py
@@ -50,8 +50,8 @@ for row in range(1, 7):
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for row in range(1, num_rows+1):
-        for column in range(1, num_columns+1):              
-            print(operation(row, column), end='\t')         
+        for column in range(1, num_columns+1):
+            print(operation(row, column), end='\t')
         print()
 
-print_operation_table(lambda x, y: x*y, 6, 6)            
+print_operation_table(lambda x, y: x*y)
